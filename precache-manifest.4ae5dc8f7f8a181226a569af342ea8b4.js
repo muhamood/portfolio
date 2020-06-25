6ce5f28b47be4afb0f47680808a96fe36d52acaf
muhamood <nmuhamood3@gmail.com>
2020-06-25 19:26:14 +0300

    Updates

---
+++ precache-manifest.4ae5dc8f7f8a181226a569af342ea8b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21ab5c08c60592bac38cc257c5066bed",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "05be3181a347df10f40f",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "596ed7de325b2e645522",
+    "url": "/portfolio/static/js/2.a5678df6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.a5678df6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05be3181a347df10f40f",
+    "url": "/portfolio/static/js/main.ad4169e4.chunk.js"
+  },
+  {
+    "revision": "5c87ae3995ec1414f89e",
+    "url": "/portfolio/static/js/runtime-main.276ddeef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
