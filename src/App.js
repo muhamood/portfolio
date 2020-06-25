1a43b324c401ef7ceccd3b887b9225e0cd2b6230
muhamood <nmuhamood3@gmail.com>
2020-06-25 22:33:30 +0300

    setting the pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import './App.css';
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+import { Link } from "react-router-dom";
+
 
 function App() {
   return (
-    <h1>hello world</h1>
+    <div style={{height: '638px', position: 'relative'}}>
+    <Layout style={{background: 'url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover'}}>
+        <Header transparent title="Title" style={{color: 'white'}}>
+            <Navigation>
+                <a href="#">Landing Page</a>
+                <a href="#">Resume</a>
+                <a href="#">About Me</a>
+                <a href="#">Contact</a>
+            </Navigation>
+        </Header>
+        <Drawer title="Title">
+            <Navigation>
+                <a href="#">Landing Page</a>
+                <a href="#">Resume</a>
+                <a href="#">About Me</a>
+                <a href="#">Contact</a>
+            </Navigation>
+        </Drawer>
+        <Content />
+    </Layout>
+</div>
   );
 }
 
---
+++ src/components/aboutme.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+export const AboutMe = () =>{
+    return(
+
+         <div><h1>About me page</h1></div>
+
+    	)
+};
